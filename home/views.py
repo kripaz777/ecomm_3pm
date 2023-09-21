3e13c65924b529c9b26cbc078abba18f62831ff0
Aayush <aceraayush@gmail.com>
2023-09-21 15:41:14 +0545

    first commit

---
+++ home/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.views.generic import View
+from .models import *
+
+# Create your views here.
+class BaseView(View):
+    views = {}
+    views['categories'] = Category.objects.all()
+    views['brands'] = Brand.objects.all()
+    views['siteinfos'] = SiteInfo.objects.all()
+
+
+class HomeView(BaseView):
+    def get(self,request):
+        self.views
+        self.views['sliders'] = Slider.objects.all()
+        self.views['ads'] = Ad.objects.all()
+        self.views['hots'] = Product.objects.filter(labels = 'hot')
+        self.views['news'] = Product.objects.filter(labels = 'new')
+        self.views['sales'] = Product.objects.filter(labels = 'sale')
+        return render(request,'index.html',self.views)
+
+
+class CategoryView(BaseView):
+    def get(self,request,slug):
+        self.views
+        cat_id = Category.objects.get(slug = slug).id
+        self.views['cat_products'] = Product.objects.filter(category_id = cat_id)
+        return render(request, 'category.html', self.views)

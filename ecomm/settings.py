b86bdf0ffba3d913c7d9ac21af37658ff43d1633
Aayush <aceraayush@gmail.com>
2023-09-29 15:59:49 +0545

    login and product revew

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -121,5 +121,7 @@ STATICFILES_DIRS = [
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- home/models.py
+++ home/models.py
@@ -71,3 +71,14 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class ProductReviews(models.Model):
+    username = models.CharField(max_length=300)
+    email = models.EmailField(max_length=100)
+    date = models.DateField(auto_now_add=True)
+    star = models.IntegerField()
+    review = models.TextField()
+    image = models.ImageField(upload_to='media',null = True)
+
+    def __str__(self):
+        return self.username
